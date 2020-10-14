2613d7f1e60eaf3606f5526097423ecd41896b45
Will <tomshardware55@gmail.com>
2020-10-14 21:56:02 +0900

    Updated Working File

--- videoDownloader.py
+++ videoDownloader.py
@@ -6,11 +6,14 @@ URL = "https://www.youtube.com/watch?v=sJ0MaudqcsY"
 #url = input(str("Enter the URL: "))
 
 
-def download():
+def download_vid():
     os.system('cmd /c youtube-dl '+ URL)
 
+def download_audio():
+    os.system('cmd /c youtube-dl --extract-audio --audio-format mp3 ' + URL)
+
 def queue():
-    f = multiprocessing.Process(target=download)
+    f = multiprocessing.Process(target=download_vid)
     f.start()
     check_status(f)
     return
@@ -27,9 +30,9 @@ def check_status(f):
 
 if __name__ == "__main__":
     root = tk.Tk()
-    canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'gray90', relief = 'raised')
+    canvas1 = tk.Canvas(root, width = 300, height = 200, bg = 'gray90', relief = 'raised')
     canvas1.pack()
-    d_button = tk.Button(master = root, text = "Download Video", command = download)
+    d_button = tk.Button(master = root, text = "Download Video", command = queue)
     d_button.pack()
     label = tk.Label(master = root, text = "Not Downloading")
     label.pack()
