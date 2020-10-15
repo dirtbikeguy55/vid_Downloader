932a4b9d11b5aec4a5d0db201745bf24b22dbb06
Will <tomshardware55@gmail.com>
2020-10-15 10:59:29 +0900

    Updated

--- videoDownloader.py
+++ videoDownloader.py
@@ -1,27 +1,56 @@
 import os
 import tkinter as tk
 import multiprocessing
+import youtube_dl
 
 #TODO: Make the URL be user input and add a text field for the input
 # Add check boxes that allow the user to select audio or video
 # Add a field for the user to select to download destination
+# Show the command line output in a text box
 # 
 # Add an advanced section that lets the user add args to the youtube-dl command
 
 URL = "https://www.youtube.com/watch?v=sJ0MaudqcsY"
-args = ""
 #url = input(str("Enter the URL: "))
+
+ydl_opts = {}
+
+var1 = int
+var2 = int
 
 #downloads the video that the user inputs
 def download_vid():
-    os.system('cmd /c youtube-dl '+ URL)
+
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp4',
+        'preferredquality': '192',
+        }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])
+    #not used
+    #os.system('cmd /c youtube-dl '+ URL)
 
 #downloads the audio from the video that the user inputs
 def download_audio():
-    os.system('cmd /c youtube-dl --extract-audio --audio-format mp3 ' + URL)
 
-def advanced():
-    os.system('cmd /c youtube-dl ' + args + ' ' + URL)
+    ydl_opts = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '192',
+        }]
+    }
+
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([URL])
+    #Not used anymore
+    #os.system('cmd /c youtube-dl --extract-audio --audio-format mp3 ' + URL)
 
 #adds multiprocessing to the command so tkinter dosent freeze while its running
 def queue():
@@ -37,6 +66,8 @@ def check_status(f):
     if f.is_alive():
         label.config(text = "Downloading")
         d_button.config(state = "disabled")
+        audiocb.config(state = "disabled")
+        videocb.config(state = "disabled")
         root.after(200, lambda f=f: check_status(f))
     else:
     #changes the label to show when the function is not running
@@ -46,13 +77,21 @@ def check_status(f):
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.geometry("400x200")
+    #root.resizable(height = 200, width = 500)
     #specifies the canvas for the application
-    canvas1 = tk.Canvas(root, width = 300, height = 200, bg = 'gray90', relief = 'raised')
-    canvas1.pack()
+    label2 = tk.Label(root, text = "Enter URL")
+    label2.pack(side = 'top')
+    urlin = tk.Entry(root,width = 45)
+    urlin.pack(side = 'top')
+    audiocb = tk.Checkbutton(root,text = 'Audio', variable=var1)
+    audiocb.pack(side = 'top')
+    videocb = tk.Checkbutton(root,text = 'Video', variable=var2)
+    videocb.pack(side = 'top')
     #creates a button that runs the function
-    d_button = tk.Button(master = root, text = "Download Video", command = queue)
-    d_button.pack()
+    d_button = tk.Button(root, text = "Download Video", command = queue)
+    d_button.pack(side = 'top')
     #creates a label for updating the user
-    label = tk.Label(master = root, text = "Not Downloading")
-    label.pack()
+    label = tk.Label(root, text = "Not Downloading")
+    label.pack(side = 'top')
     root.mainloop()
